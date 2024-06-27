f593c2e962e33b7ba940fbfd1a19627c92a38d17
CrissOlivares <cri.olivaresh@duocuc.cl>
2024-06-27 15:27:02 -0400

    Instala Django Bv5

--- myvenv/Scripts/activate.bat
+++ myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Miguel Astorga\Downloads\GroundZero-master1\GroundZero-master1\myvenv
+set VIRTUAL_ENV=C:\Users\coh_o\Desktop\GroundZero-master1\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
